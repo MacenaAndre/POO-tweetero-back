253ed7eef9e9225e17b2b32fc3702271b82e54c2
André Macêna <107252160+MacenaAndre@users.noreply.github.com>
2023-02-27 17:45:40 -0300

    adjustment GET /tweets/:username

--- src/routers/app.router.js
+++ src/routers/app.router.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { postSignUp } from "../controllers/sign.up.controller.js";
-import { postTweet } from "../controllers/tweet.controller.js";
+import { findUserTweets, postTweet } from "../controllers/tweet.controller.js";
 
 const appRouter = Router();
 
 appRouter.post("/sign-up", postSignUp),
 appRouter.post("/tweets", postTweet),
-appRouter.get("/tweets/:username");
+appRouter.get("/tweets/:username", findUserTweets);
 
 export { appRouter };
